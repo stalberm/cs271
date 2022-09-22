1d68dda45c5f93c1ca22d32dc5d2a7e675b1a80c
Micah Stalberg <stalberm@oregonstate.edu>
2022-09-22 08:32:46 -0700

    Move cplorations into repo

---
+++ cplorations/c00/main.c
@@ -0,0 +1,13 @@
+/****************************************
+ * C-ploration 0 for CS 271
+ *
+ * [NAME] Micah Stalberg
+ * [TERM] FALL 2022
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
