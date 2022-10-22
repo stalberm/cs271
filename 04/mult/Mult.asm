1a4335ebbb1deca54de341b7ef9cf591ae3e8a84
Micah Stalberg <stalberm@oregonstate.edu>
2022-10-21 17:03:27 -0700

    Completes Mult assembly program

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,47 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+//Initialize product to 0
+@2
+M=0
+
+//If r0 is 0, product r2 will still be 0
+@0
+D=M
+@END
+D;JEQ
+
+//If r1 is 0, product r2 will still be 0
+@1
+D=M
+@END
+D;JEQ
+
+//Get a counter variable in RAM3 equal to our second value
+//So we will add r0 to our product r1 times
+@1
+D=M
+@3
+M=D
+
+(LOOP)
+//Get r0
+@0
+D=M
+
+//Add r0 to the product
+@2
+M=M+D
+
+//Decrement our counter
+@3
+M=M-1
+
+//If our counter variable is greater than 0 keep looping
+D=M
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
