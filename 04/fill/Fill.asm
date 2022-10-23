18608f384624da7571d683ff9e180234a4903f5e
Micah Stalberg <stalberm@oregonstate.edu>
2022-10-23 14:29:13 -0700

    Completes Fill program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,77 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+@SCREEN
+//Capture screen location in D
+D=A
+@R0
+//Store screen location in RAM[0]
+M=D
+
+//256 rows of 32 16 bit words - 256x32 = 8192
+//SCREEN starts at 16384 and KBD starts at 24576 - 8192 from SCREEN to KBD
+//Store the number of fills needed for the screen in RAM[1]
+@8192
+D=A
+@R1
+M=D
+
+//Get keyboard input, fillwhite if 0, else fill black
+(INPUT)
+@KBD
+D=M
+@FILLWHITE
+D;JEQ
+@FILLBLACK
+D;JGT
+
+//Infinite loop to check for KBD input
+@INPUT
+0;JMP
+
+
+(FILLWHITE)
+D=0
+//Get current screen address out of RAM[0]
+@R0
+A=M
+//Set the value of that address to 0 - white
+M=D
+//Filled another piece, so update current screen address to current val plus 1
+@R0
+M=M+1
+
+//A piece was filled, so decrement our counter 
+@R1
+M=M-1
+D=M
+
+//If the counter is 0, we filled the screen so repeat if not filled
+@FILLWHITE
+D;JGT
+
+@START
+0;JMP
+
+(FILLBLACK)
+D=-1
+//Get current screen address out of RAM[0]
+@R0
+A=M
+//Set the value of that address to -1 - black
+M=D
+//Filled another piece, so update current screen address to current val plus 1
+@R0
+M=M+1
+
+//A piece was filled, so decrement our counter 
+@R1
+M=M-1
+D=M
+//If the counter is 0, we filled the screen so repeat if not filled
+@FILLBLACK
+D;JGT
+
+@START
+0;JMP
